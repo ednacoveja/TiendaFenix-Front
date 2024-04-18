090e25451e2138a5187d8e7f5a9e46a826b072bc
ednacoveja <ovejerocande@gmail.com>
2024-04-18 16:03:49 -0300

    cambio en la url del back

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import axios from "axios";
 import { QueryClient, QueryClientProvider } from 'react-query'
 
 const queryClient = new QueryClient()
-axios.defaults.baseURL = "https://back-fenix.onrender.com/";
+axios.defaults.baseURL = "https://back-fenix.onrender.com";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
