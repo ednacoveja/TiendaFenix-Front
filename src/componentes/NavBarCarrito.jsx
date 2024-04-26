f2c2d562f672380243e6a8630cfff77cf8903e39
ednacoveja <ovejerocande@gmail.com>
2024-04-26 14:38:45 -0300

    info no  mas

--- src/componentes/NavBarCarrito.jsx
+++ src/componentes/NavBarCarrito.jsx
@@ -60,7 +60,7 @@ export default function Navbar() {
                     </Typography>
 
                     <Typography variant="h7" component="div" sx={{ flexGrow: 1 }}>
-                        Martes a Sábados de 12 a 15hs en Balcarce 427
+                        Miércoles y Jueves de 12 a 15hs en Balcarce 427
                     </Typography>
                     <Typography>
                         <Button color="inherit" component="button" variant="outline" onClick={(e) => {
